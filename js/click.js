5bc4a7e266a6d7e8be5f7cdd130f63bbf4afaeb7
jusmac <justyna.machalica@gmail.com>
2015-06-10 14:45:22 +0200

    galeria, poprawki tla i obrazkow

--- js/click.js
+++ js/click.js
@@ -1,20 +1,3 @@
-var slideMenu = function() {
-    $(document).ready(function() {
-        $('.submenu').addClass('submenu-li-none');
-        console.log('ok');
-    });
-    $('.menu-li').mouseenter(function() {
-        $(this).children('.submenu').removeClass('submenu-li-none');
-        $(this).children('submenu').addClass('submenu-li');
-        console.log('dupa');
-    });
-    $('.menu-li').mouseleave(function() {
-        $(this).children('.submenu').removeClass('submenu-li');
-        $(this).children('.submenu').addClass('submenu-li-none');
-    });
-};
-
 
 $(function() {
-    slideMenu();
 });
